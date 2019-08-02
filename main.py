21d015785f310188a88cef69c43406aae23bb282
ClockworkKettle <timothyjmcnamara@gmail.com>
2019-08-02 15:57:14 +0100

    Removed extra .pos

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ os.system("./teqc -sep sbf %s > %s" %(args.rover_in, args.rover_in+".rinex"))
 rover_rinex = args.rover_in+".rinex"
 base_stn_loc = args.base_stn
 #Perform DGNSS corrections and output to POS file
-os.system("app/rnx2rtkp/gcc/rnx2rtkp %s %s -c on -o %s.pos"
-            %(rover_rinex, base_stn_loc.split(".")[0]+"*", args.rover_in.split("."[0]+".pos")))
+os.system("app/rnx2rtkp/gcc/rnx2rtkp %s %s -c on -o %s"
+    %(rover_rinex, base_stn_loc.split(".")[0]+"*", args.rover_in.split(".")[0]+".pos"))
